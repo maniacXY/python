4a0f6cd912e3638ab96329592e0b13eff60f5f13
Pad <maniacxy@protonmail.com>
2022-05-05 15:15:27 +0200

    some changes

--- cookie_challenge/firstcookie.py
+++ cookie_challenge/firstcookie.py
@@ -1,45 +1,49 @@
-# Upgrades: id=upgrade0 -> class="crate upgrade enabled"
-# max products: product17 -> class="product locked disabeled/enabled"
 from selenium import webdriver  
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time 
 from time import sleep
-  
+
 class FirstCookie:
     def __init__(self) -> None:
+        self.firstround = 0
         self.driver = webdriver.Firefox()
-        
-    def clicking(self,zeit):
+        self.driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+    def clicking(self, clickerzeit=20):
         self.cookie = self.driver.find_element(By.ID, "bigCookie")
         actual_time = time.time()
-        while time.time() - actual_time < zeit: 
+        while time.time() - actual_time < clickerzeit: 
             self.cookie.click()
         return False
 
-    def clickerloop(self, zeit_between_upgrades):
-        
-        self.driver.get("https://orteil.dashnet.org/cookieclicker/")
-        sleep(3)
-        a = 0
-        while True:
-            if self.clicking(zeit_between_upgrades) == False:
-                if a == 1:
-                    upgrade = self.driver.find_element(By.ID, "upgrade0").get_attribute("class")
-                    if "enabled" in upgrade:
-                        clicker = self.driver.find_element(By.ID, "upgrade0")
-                        clicker.click()
+    def clickerloop(self):
+        if self.firstround == 1:
+            upgrade = self.driver.find_element(By.ID, "upgrade0").get_attribute("class")
+            if "enabled" in upgrade:
+                clicker = self.driver.find_element(By.ID, "upgrade0")
+                clicker.click()
+        for i in range(18):
+            element = self.driver.find_element(By.ID, f"product{17-i}").get_attribute("class")
                 
+            if "enabled" in element:
+                clicker = self.driver.find_element(By.ID, f"product{17-i}")
+                clicker.click()
+                a = 1
+                break
+        self.firstround = 1
 
-                for i in range(18):
-                    element = self.driver.find_element(By.ID, f"product{17-i}").get_attribute("class")
-                        
-                    if "enabled" in element:
-                        clicker = self.driver.find_element(By.ID, f"product{17-i}")
-                        clicker.click()
-                        a = 1
-                        break
-                        
-                    else:
-                        continue
-    
+
+delay = 180
+
+
+
+cookie = FirstCookie()
+sleep (10)
+now = time.time() + delay
+while time.time() < now: 
+    cookie.clicking(15)
+    cookie.clickerloop()
+
+print("Ready ")
+
